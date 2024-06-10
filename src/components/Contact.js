4fc02810db821ca6a4dd27409dfcdc2e7cbf2f58
brabije yassine <brabije.yassine123@gmail.com>
2024-06-11 00:34:47 +0100

    contact last

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -3,45 +3,28 @@ import axios from 'axios';
 import Contactbg from '../image/contact.png';
 import villesData from './Villes.json';
 
-
-
 function Contact() {
     const [fullName, setFullName] = useState('');
     const [email, setEmail] = useState('');
     const [phoneNumber, setPhoneNumber] = useState('');
-    const [ville, setville] = useState('');
-    const [villes, setvilles] = useState([]);
-    const [isPhoneNumberValid, setIsPhoneNumberValid] = useState(true);
+    const [ville, setVille] = useState('');
+    const [villes, setVilles] = useState([]);
     const [isEmailValid, setIsEmailValid] = useState(true);
     const [showAlert, setShowAlert] = useState(false);
 
-
-
     useEffect(() => {
-        setvilles(villesData);
+        setVilles(villesData);
     }, []);
 
-    const validatePhoneNumber = (number) => {
-        const phoneRegex = /^\d{10}$/;
-        return phoneRegex.test(number);
-    };
 
-    const validateEmail = (email) => {
-        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-        return emailRegex.test(email);
-    };
 
-    const handlePhoneNumberChange = (e) => {
+    const handlePhoneChange = (e) => {
         const value = e.target.value;
-        setPhoneNumber(value);
-        setIsPhoneNumberValid(validatePhoneNumber(value));
+        const numericValue = value.replace(/\D/g, '').slice(0, 10);
+        setPhoneNumber(numericValue);
     };
 
-    const handleEmailChange = (e) => {
-        const value = e.target.value;
-        setEmail(value);
-        setIsEmailValid(validateEmail(value));
-    };
+
 
     const handleSubmit = async (e) => {
         e.preventDefault();
@@ -49,32 +32,22 @@ function Contact() {
             email,
             fullName,
         };
-        if (!isPhoneNumberValid || !isEmailValid) {
-            return;
-        }
 
-        try {
+        await axios.post('http://localhost:8000/api/inscriptions', {
+            full_name: fullName,
+            email: email,
+            phone_number: phoneNumber,
+            ville: ville,
+        });
+        setShowAlert(true);
+        setTimeout(() => {
+            setShowAlert(false);
+        }, 4000);
 
-            await axios.post('http://localhost:8000/api/inscriptions', {
-                full_name: fullName,
-                email: email,
-                phone_number: phoneNumber,
-                ville: ville,
-            });
-            setShowAlert(true);
-            setTimeout(() => {
-                setShowAlert(false);
+        await axios.post('http://localhost:8000/api/send-email', emailData);
 
-            }, 4000);
+        window.location.href = '/';
 
-            await axios.post('http://localhost:8000/api/send-email', emailData);
-
-            window.location.href = '/';
-
-
-        } catch (error) {
-            console.error(error);
-        }
     };
 
     return (
@@ -106,20 +79,18 @@ function Contact() {
                                         <input
                                             type="tel"
                                             value={phoneNumber}
-
-                                            onChange={handlePhoneNumberChange}
-                                            className={`bg-[#000] border ${isPhoneNumberValid ? 'border-gray-300' : 'border-red-500'} text-white text-sm rounded-lg block w-full p-4`}
+                                            onChange={handlePhoneChange}
+                                            className="bg-[#000] border border-gray-300 text-white text-sm rounded-lg block w-full p-4"
                                             placeholder="Phone Number..."
                                             required
                                         />
-
                                     </div>
                                 </div>
                                 <input
                                     type="email"
                                     value={email}
-                                    onChange={handleEmailChange}
-                                    className={`bg-[#000] mb-5 border ${isEmailValid ? 'border-gray-300' : 'border-red-500'} text-white text-sm rounded-lg block w-full p-4 focus:border-red-500`}
+                                    onChange={(e) => setEmail(e.target.value)}
+                                    className="bg-[#000] border mb-5 border-gray-300 text-white text-sm rounded-lg block w-full p-4"
                                     placeholder="Your Email Address..."
                                     required
                                 />
@@ -127,17 +98,17 @@ function Contact() {
                                 <div className="mb-5">
                                     <select
                                         value={ville}
-                                        onChange={(e) => setville(e.target.value)}
-                                        className="bg-[#000] border border-gray-300 text-white text-sm p-4 rounded-lg block w-full "
+                                        onChange={(e) => setVille(e.target.value)}
+                                        className="bg-[#000] border border-gray-300 text-white text-sm p-4 rounded-lg block w-full"
                                         required
                                     >
-                                        <option value="">Select Ville </option>
+                                        <option value="">Select Ville</option>
                                         {villes.map((ville) => (
-                                            <option key={ville.id} value={ville.ville} className='text-[#fff]' >{ville.ville}</option>
+                                            <option key={ville.id} value={ville.ville} className='text-[#fff]'>{ville.ville}</option>
                                         ))}
                                     </select>
                                 </div>
-                                <button type="submit" className="text-[#FF0000] border border-[#FF0000] hover:bg-[#FF0000] hover:text-[#FFF] py-2 px-8  tracking-wider transition duration-200">
+                                <button type="submit" className="text-[#FF0000] border border-[#FF0000] hover:bg-[#FF0000] hover:text-[#FFF] py-2 px-8 tracking-wider transition duration-200">
                                     Send
                                 </button>
                             </form>
@@ -149,27 +120,24 @@ function Contact() {
                 <div className="fixed inset-0 z-50 flex justify-center items-center w-full h-full bg-black bg-opacity-50 backdrop-blur-sm">
                     <div className="bg-white rounded-lg">
                         <div className="mt-3 text-center">
-                            <div className="mx-auto flex items-center justify-center h-12 w-12 rounded-full bg-white border border-[#FF0000] ">
-                                <svg className="h-6 w-6 " fill="none" stroke="#FF0000" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                            <div className="mx-auto flex items-center justify-center h-12 w-12 rounded-full bg-white border border-[#FF0000]">
+                                <svg className="h-6 w-6" fill="none" stroke="#FF0000" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                                     <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 13l4 4L19 7"></path>
                                 </svg>
                             </div>
                             <h3 className="text-lg leading-6 font-medium text-gray-900 mt-3">Successfull</h3>
-                            <div className=" px-7 py-3">
+                            <div className="px-7 py-3">
                                 <p className="text-sm font-medium text-[#000] tracking-wides">Your inscription has been successfully .</p>
                             </div>
-                            <div className="items-center px-4 py-3 ">
-
-                                <a href="/" onClick={() => setShowAlert(false)} className="px-4 py-2 bg-[#FF0000] text-white text-base font-medium rounded-md w-96 shadow-sm  ">
+                            <div className="items-center px-4 py-3">
+                                <a href="/" onClick={() => setShowAlert(false)} className="px-4 py-2 bg-[#FF0000] text-white text-base font-medium rounded-md w-96 shadow-sm">
                                     OK
                                 </a>
-
                             </div>
                         </div>
                     </div>
                 </div>
             )}
-
         </div>
     );
 }
